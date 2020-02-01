3e9b1b566c712c86e2b11372af041cb53cb62dbf
kananek mongkolsawat <kananek.m@kkumail.com>
2020-02-01 19:29:08 +0700

    Updates

---
+++ precache-manifest.65422e44d5b58fad9ae88c07183aecbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24898c1690a4bb2fbaa712ee449e131d",
+    "url": "/nunee/index.html"
+  },
+  {
+    "revision": "c38f6d880eb3255c09c0",
+    "url": "/nunee/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8bb57503af6dda503a4e",
+    "url": "/nunee/static/js/2.04bbf51a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/nunee/static/js/2.04bbf51a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c38f6d880eb3255c09c0",
+    "url": "/nunee/static/js/main.a9510013.chunk.js"
+  },
+  {
+    "revision": "4b4e8a7ccfd761186bf1",
+    "url": "/nunee/static/js/runtime-main.b9a2ba67.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nunee/static/media/logo.5d5d9eef.svg"
+  }
+]);
